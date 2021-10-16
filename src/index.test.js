d36d8513a4d776c2d35c712f21c034109f0fcd06
CharlieJhonSmith <charlie.alfrido@gmail.com>
2021-10-16 17:09:11 +0000

    add test

--- src/index.test.js
+++ src/index.test.js
@@ -1,3 +1,15 @@
+const index = require("./index")
 it('should jest works', () => {
   expect(1 + 1).toEqual(2)
+})
+
+// @ponicode
+describe("index.babelWorksWithExports", () => {
+    test("0", () => {
+        let callFunction = () => {
+            index.babelWorksWithExports()
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
 })
